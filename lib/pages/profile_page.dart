7769d17476c8076958b46afdc2450eb52feb566d
RupakO4 <47048146+RupakO4@users.noreply.github.com>
2022-01-27 20:46:37 +0545

    profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,24 +5,190 @@ class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Material(
-      color: context.canvasColor,
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Profile",
-                style: Theme.of(context).textTheme.headline1,
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       body: Material(
+  //     color: context.canvasColor,
+  //     child: Column(
+  //       children: [
+  //         Padding(
+  //           padding: EdgeInsets.all(24.0),
+  //           child: Align(
+  //             alignment: Alignment.topLeft,
+  //             child: Text(
+  //               "Profile",
+  //               style: Theme.of(context).textTheme.headline1,
+  //             ),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   ));
+  // }
+   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: context.canvasColor,
+         title: Text('Profile'),
+        centerTitle: true,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.blue,
+                    Colors.blue.shade100
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  stops: [0.5, 0.9],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white70,
+                        minRadius: 60.0,
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage:
+                              AssetImage('assets/images/logo_with_text.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Rupak Raj Pantha',//name
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            Container(
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      'Email',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'palmeiro.leonardo@gmail.com',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text(
+                      'College',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Pulchowk Campus',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text(
+                      'Roll No',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      '075bct070',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Field of Study',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Computer Engineering',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Degree',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Bachelor in Engineering',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Contact No',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      '9811111111',
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
+//reference:https://medium.com/@palmeiro.leonardo/simple-profile-screen-with-flutter-fe2f1f7cfaf5
